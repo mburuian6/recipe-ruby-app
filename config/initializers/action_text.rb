784d59da62f8ccaab3b111c22fc994c0c065de45
mburuian6 <mburuian6@gmail.com>
2021-10-11 01:56:36 +0300

    actiontext hook added->application.css.scss hook added

--- config/initializers/action_text.rb
+++ config/initializers/action_text.rb
@@ -1,4 +1,4 @@
-ActiveSupport.on_load(:action_view) do
-    include ActionText::ContentHelper
-    include ActionText::TagHelper
-end
+# ActiveSupport.on_load(:action_view) do
+#     include ActionText::ContentHelper
+#     include ActionText::TagHelper
+# end
