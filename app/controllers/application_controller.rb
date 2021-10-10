a8611bbef64264a93b0eb5da95ac0988c5cb02fb
mburuian6 <mburuian6@gmail.com>
2021-10-11 01:21:53 +0300

    added a helpers hook in app_controller.rb to ensure ActionText works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
+require 'action_text'
+
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+  helpers ActionText::Engine.helpers
+  before_action :authenticate_user!
 
-    protect_from_forgery with: :exception
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
-      def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :user_name, :name, :password])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:email, :user_name, :name, :password, :current_password])
-      end
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :user_name, :name, :password])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:email, :user_name, :name, :password, :current_password])
+    end
 
 end
