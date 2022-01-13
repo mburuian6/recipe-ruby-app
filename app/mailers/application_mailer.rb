06e1b9d670062a2d140e0313f4a56c91689f2181
Ian Mburu <mburu@finplusgroup.com>
2022-01-13 12:50:53 +0300

    send name and email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: email_address_with_name(
+    Rails.application.credentials.default_mail_from,
+    'Our Company'
+  )
   layout 'mailer'
 end
